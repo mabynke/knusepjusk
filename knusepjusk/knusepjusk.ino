917240f0cb9126a950ace10ed4353ab528292cd4
Marvin Reza <marvinreza@outlook.com>
2017-03-14 18:24:18 +0100

    Bluetooth implementert og JavaFX kode lagt til

--- knusepjusk/knusepjusk.ino
+++ knusepjusk/knusepjusk.ino
@@ -1,24 +1,111 @@
-#include <ZumoReflectanceSensorArray.h>
-#include <ZumoMotors.h>
+/*
+ */
+#include <NewPing.h>
+#include <NewServo.h>
 #include <Pushbutton.h>
+#include <PLab_ZumoMotors.h>
+#include <ZumoMotors.h>
+#include <QTRSensors.h>
+#include <ZumoReflectanceSensorArray.h>
 
-ZumoReflectanceSensorArray irSensors(QTR_NO_EMITTER_PIN);
+// this might need to be tuned for different lighting conditions, surfaces, etc.
+#define QTR_THRESHOLD  1800 // 
+  
+// these might need to be tuned for different motor types
+int REVERSE_SPEED  = 200; // 0 is stopped, 400 is full speed
+int TURN_SPEED     = 200;
+int FORWARD_SPEED  = 200;
+
+#define NUM_SENSORS 6
+unsigned int sensor_values[NUM_SENSORS];
+
+#define SERVO_OFFSET 20  // DS: Degrees offset of servo... error in hardware...
+ 
+ZumoReflectanceSensorArray sensors;
+
+
 ZumoMotors motors;
-Pushbutton button(ZUMO_BUTTON);
 
-unsigned int sensorData[6];
+const int ledPin=A4;
+
+const int echoPin = A1;
+const int triggerPin = A1;
+const int maxDistance = 50;
+
+const int servoPin = 6;
+
+NewPing sonar(triggerPin, echoPin, maxDistance);
+NewServo myServo; 
+
+Pushbutton button(ZUMO_BUTTON); 
+
+int degreesServo = 0;
+int degreesStep = 10;
+
+PLab_ZumoMotors plab_Motors;
 
 void setup() {
+  sensors.init(QTR_NO_EMITTER_PIN);  // 
   Serial.begin(9600);
-  button.waitForButton();
+  pinMode(ledPin,OUTPUT);
+  myServo.attach(servoPin); 
+  myServo.write(90);
+  button.waitForButton(); // start when button pressed
 }
 
-void loop() {
-  irSensors.read(sensorData);
-  for (auto it : sensorData) {
-    Serial.print(it);
-    Serial.print(' ');
-  }
-  Serial.println();
-  delay(200);
+void stepServo() {
+   degreesServo = degreesServo + degreesStep;
+   if (degreesServo > 180) {
+       degreesStep = -degreesStep;
+       degreesServo = 180;
+   } else if (degreesServo < 0) {
+       degreesStep = -degreesStep;
+       degreesServo = 0;
+   } 
+   myServo.write(degreesServo);
 }
+
+float sonarDistance() {
+  // Gjør ett ping, og beregn avstanden
+  unsigned int time = sonar.ping();
+  float distance = sonar.convert_cm(time);
+  if (distance == 0.0) { // sonar gives zero when outside range
+    // Turn off LED and just go forward
+    digitalWrite(ledPin,LOW); 
+   } else {
+    digitalWrite(ledPin,HIGH);
+   }
+   return distance;
+}
+
+
+void loop() {
+   stepServo(); 
+   int distance = sonarDistance(); 
+   if (distance > 0) {
+      int actual_degrees_servo = degreesServo + SERVO_OFFSET;
+      if (actual_degrees_servo > 90) {
+         plab_Motors.turnLeft(TURN_SPEED,actual_degrees_servo-90);
+         degreesServo = 90 - SERVO_OFFSET;
+      } else if (actual_degrees_servo < 90) {
+         plab_Motors.turnRight(TURN_SPEED,90-actual_degrees_servo);
+         degreesServo = 90 - SERVO_OFFSET;
+      };
+      myServo.write(degreesServo);
+    }
+   sensors.read(sensor_values);  
+   if (sensor_values[0] < QTR_THRESHOLD) {
+     plab_Motors.backward(REVERSE_SPEED, 10);
+     plab_Motors.turnRight(TURN_SPEED,90);
+   } else if (sensor_values[5] < QTR_THRESHOLD) {
+     plab_Motors.backward(REVERSE_SPEED, 10);
+     plab_Motors.turnLeft(TURN_SPEED,90);
+   }
+   else
+   {
+    // otherwise, go straight
+     motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  } 
+}
+
+
