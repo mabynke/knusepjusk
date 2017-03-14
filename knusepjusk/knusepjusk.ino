f2ed0c836ebdcac1d0965581660111c4e87765c4
Marvin Reza <marvinreza@outlook.com>
2017-03-14 18:31:07 +0100

    La til riktig fil nå

--- knusepjusk/knusepjusk.ino
+++ knusepjusk/knusepjusk.ino
@@ -7,6 +7,8 @@
 #include <ZumoMotors.h>
 #include <QTRSensors.h>
 #include <ZumoReflectanceSensorArray.h>
+#include <SoftwareSerial.h>
+#include <PLabBTSerial.h>
 
 // this might need to be tuned for different lighting conditions, surfaces, etc.
 #define QTR_THRESHOLD  1800 // 
@@ -28,11 +30,16 @@ ZumoMotors motors;
 
 const int ledPin=A4;
 
-const int echoPin = A1;
-const int triggerPin = A1;
+const int echoPin = 2;
+const int triggerPin = 3;
 const int maxDistance = 50;
 
 const int servoPin = 6;
+
+const int txPin = 0; // Connected to tx on bt unit
+const int rxPin = 1; // Connected to rx on bt unit
+
+PLabBTSerial btSerial(txPin, rxPin);
 
 NewPing sonar(triggerPin, echoPin, maxDistance);
 NewServo myServo; 
@@ -44,6 +51,44 @@ int degreesStep = 10;
 
 PLab_ZumoMotors plab_Motors;
 
+//...........................................................................
+// Always include this method.
+// It reads from the BT port and calls BTSerialMessageReceived.
+// 
+char msg[100];
+void updateBTSerial() {
+  int availableCount = btSerial.available();
+  if (availableCount > 0) {
+    btSerial.read(msg, availableCount);
+    char *divided = strchr(msg,',');
+    int msgValue = 0;
+    if (divided != 0) {
+       divided[0] = 0; divided++;
+       String str(divided);
+       msgValue = str.toInt();
+    };
+    String msgString(msg);
+    BTSerialMessageReceived(msgString,msgValue);   
+  }
+}
+//...........................................................................
+
+//...........................................................................
+// Always include these two methods .
+// They send a message to the BT port, without or with an int value
+// 
+void BTSerialSendMessage(String msgString) {
+  btSerial.println(msgString); 
+}
+
+void BTSerialSendMessage(String msgString,int msgValue) {
+  btSerial.print(msgString); 
+  btSerial.print(",");
+  btSerial.println(msgValue);
+}
+//...........................................................................
+
+
 void setup() {
   sensors.init(QTR_NO_EMITTER_PIN);  // 
   Serial.begin(9600);
@@ -51,6 +96,7 @@ void setup() {
   myServo.attach(servoPin); 
   myServo.write(90);
   button.waitForButton(); // start when button pressed
+  btSerial.begin(9600); // Open serial communication to Bluetooth unit
 }
 
 void stepServo() {
@@ -78,17 +124,31 @@ float sonarDistance() {
    return distance;
 }
 
+void BTSerialMessageReceived(String msgString,int msgValue) {
+  Serial.print("Message:"); Serial.print(msgString); // Debug print
+  Serial.print(", Value:"); Serial.println(msgValue);  // Debug print
+  if (msgString == "#speed") {
+    REVERSE_SPEED  = msgValue; // 0 is stopped, 400 is full speed
+    TURN_SPEED     = msgValue;
+    FORWARD_SPEED  = msgValue; return;
+  } 
+}
 
 void loop() {
+   updateBTSerial();  // Check if we have input on the BT serial port.
    stepServo(); 
    int distance = sonarDistance(); 
    if (distance > 0) {
       int actual_degrees_servo = degreesServo + SERVO_OFFSET;
       if (actual_degrees_servo > 90) {
          plab_Motors.turnLeft(TURN_SPEED,actual_degrees_servo-90);
+         BTSerialSendMessage("#distance",distance);       
+         BTSerialSendMessage("#angle",actual_degrees_servo);
          degreesServo = 90 - SERVO_OFFSET;
       } else if (actual_degrees_servo < 90) {
          plab_Motors.turnRight(TURN_SPEED,90-actual_degrees_servo);
+         BTSerialSendMessage("#distance",distance);       
+         BTSerialSendMessage("#angle",actual_degrees_servo);
          degreesServo = 90 - SERVO_OFFSET;
       };
       myServo.write(degreesServo);
